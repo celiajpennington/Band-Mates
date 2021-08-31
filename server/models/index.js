1f48b9381d6f371afc5e90c81c1fecff0ed29697
Zhansaya Sarmanova <zhansayasarmanova1993@gmail.com>
2021-08-31 14:46:51 -0500

    created models

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
-const Product = require('./Product');
+const Product = require('./Comment');
 const Category = require('./Category');
+const Post = require('./Post')
 const Order = require('./Order');
 
-module.exports = { User, Product, Category, Order };
+module.exports = { User, Product, Category, Order, Post };
