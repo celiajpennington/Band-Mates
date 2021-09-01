ac7f479b4e7ecf1e1edde465a1352b0c0b6fb50f
Celia Pennington <celiajpennington@gmail.com>
2021-09-01 09:59:46 -0500

    added homepage structure

--- client/src/components/NavTabsHome.js
+++ client/src/components/NavTabsHome.js
@@ -1,11 +1,22 @@
 // Nav Tabs for the home page only. Will contain "About, Create Account, Log In". Create Account and Log In will redirect to another page, About will redirect to the bottome of the page where the "About Me" section will live.
 import React from 'react';
 
+
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function NavTabsHome({ currentPage, handlePageChange }) {
   return (
     <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('about')}
+          // Check to see if the currentPage is `create post`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'about' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
       <li className="nav-item">
         <a
           href="#login"
